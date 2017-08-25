8772dbfd81cb3abf3f36b60333236feb92d4a1ea
Adam Duston <aduston@gmail.com>
2017-08-25 10:39:57 -0400

    adding circular buffer to assist with n-gram calcs

--- ch4.py
+++ ch4.py
@@ -1,5 +1,21 @@
 from tokenizer import tokenize
 
+class CircularBuffer(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.pointer = 0
+        self.data = []
+
+    def add(self, obj):
+        if len(self.data) < self.capacity:
+            self.data.append(obj)
+        else:
+            self.data[self.pointer] = obj
+            self.pointer = (self.pointer + 1) % self.capacity
+
+    def make_snapshot_tuple(self):
+        pass
+        
 
 def compute_n_gram(fn, n):
     """
