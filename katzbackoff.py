6cf0d54a05a80ea8b3d37c89761bc22caabe6ead
Adam Duston <aduston@gmail.com>
2017-09-15 07:50:17 -0400

    Simplify trie nodes by adding degenerate root node, write tests and make them pass.

--- katzbackoff.py
+++ katzbackoff.py
@@ -1,4 +1,6 @@
-from ch4 import CircularBuffer
+from ch4 import CircularBuffer, CountFrequency, \
+    simple_linear_regression, SimpleGoodTuringCountSmoother
+import math
 from tokenizer import tokenize, detokenize
 import pprint
 
@@ -33,8 +35,11 @@ class LanguageModel(object):
                     return self.p_katz(n_gram[1:])
 
 class KatzTrieNode(object):
-    def __init__(self, n_gram, parent=None):
-        self.n_gram = n_gram
+    def __init__(self, n_gram=None, parent=None):
+        if n_gram is None:
+            self.n_gram = () # root trie node => empty tuple n-gram
+        else:
+            self.n_gram = n_gram
         self.parent = parent
         self.count = 0
         self.log_alpha = None
@@ -44,7 +49,8 @@ class KatzTrieNode(object):
     def populate(self, N_gram):
         self.count += 1
         if len(N_gram) > len(self.n_gram):
-            descendant = self.descendants.get(N_gram[len(self.n_gram)], None)
+            suffix = N_gram[len(self.n_gram):]
+            descendant = self.descendants.get(suffix[0], None)
             if descendant is None:
                 descendant = self.descendants.setdefault(
                     suffix[0], KatzTrieNode(N_gram[:len(self.n_gram) + 1], self))
@@ -68,77 +74,75 @@ class KatzTrieNode(object):
         return self.discounter(self.count)
 
     def p_star(self):
-        if self.parent is None:
-            # TODO: throw exception
-            pass
-        else:
-            return self.c_star() / self.parent.count
+        return self.c_star() / self.parent.count
+
+    def log_p_star(self):
+        return math.log(self.c_star()) - math.log(self.parent.count)
 
     def beta(self):
         return 1 - sum(d.p_star() for d in self.descendants.values())
 
     def find_node(self, n_gram):
-        if len(n_gram) == 1:
+        if len(n_gram) == 0:
+            return self
+        elif len(n_gram) == 1:
             return self.descendants[n_gram[0]]
         else:
             return self.descendants[n_gram[0]].find_node(n_gram[1:])
 
-def _add_discounts(trie_nodes, N):
+def _add_discounts(trie_node, N):
     for n in range(1, N + 1):
         gt_counts = {} # map of c to N_c
-        for trie_node in trie_nodes.values():
-            trie_node.populate_count_frequencies(gt_counts, n)
+        trie_node.populate_count_frequencies(gt_counts, n)
         count_frequencies = [CountFrequency(r, N_r) for r, N_r in gt_counts.items()]
         a, b = simple_linear_regression(count_frequencies)
-        discounter = SimpleGoodTuringCountSmoother(b)
-        for trie_node in trie_nodes.values():
-            trie_node.set_discounter(discounter, n)
+        trie_node.set_discounter(SimpleGoodTuringCountSmoother(b), n)
 
-def _add_alphas(trie_nodes, N):
-    stack = list(trie_nodes.values())
+def _add_alphas(trie_node, N):
+    stack = [trie_node]
     num_tokens = sum(tn.count for tn in stack)
     while len(stack) > 0:
         node = stack.pop()
-        beta_n_1 = node.beta()
-        suffix = node.n_gram[1:]
-        if len(suffix) == 1:
-            beta_n_2 = 1 - trie_nodes[suffix[0]].c_star() / num_tokens
-        else:
-            beta_n_2 = trie_nodes[suffix[0]].find_node(suffix[1:]).beta()
-        node.log_alpha = math.log(beta_n_1) - math.log(beta_n_2)
+        if len(node.n_gram) > 0:
+            beta_n_1 = node.beta()
+            suffix_node = trie_node.find_node(node.n_gram[1:])
+            denominator = 1 - sum(d.p_star() for token, d
+                                  in suffix_node.descendants.items()
+                                  if token in node.descendants)
+            node.log_alpha = math.log(beta_n_1) - math.log(denominator)
         for descendant in node.descendants.values():
-            stack.push(descendant)
+            stack.append(descendant)
 
-def make_trie_nodes(f, N, include_punctuation=False, trie_nodes=None):
+def populate_trie_nodes(f, N, include_punctuation=False, trie_node=None):
+    if trie_node is None:
+        trie_node = KatzTrieNode()
     circ_buff = CircularBuffer(N)
-    if trie_nodes is None:
-        trie_nodes = {} # map of first token prefix to root KatzTrieNode for that token.
     circ_buff.add("<s>")
+    n_gram = None
     for token in tokenize(f, include_punctuation=include_punctuation):
         circ_buff.add(token)
         if len(circ_buff) == N:
             n_gram = circ_buff.make_snapshot_tuple()
-            node = trie_nodes.get(n_gram[0], None)
-            if node is None:
-                node = trie_nodes.setdefault(n_gram[0], KatzTrieNode(n_gram))
-            node.populate(n_gram)
-    return trie_nodes
+            trie_node.populate(n_gram)
+    for i in range(1, len(n_gram)):
+        trie_node.populate(n_gram[i:])
+    return trie_node
 
 def compute_trie_nodes_for_dir(dir_name, N, include_punctuation=False):
-    trie_nodes = {}
+    trie_node = KatzTrieNode()
     for fn in os.listdir(dir_name):
         if fn == '.DS_Store':
             continue
         full_fn = os.path.join(dir_name, fn)
         with open(full_fn, 'r') as f:
-            make_trie_nodes(f, N, include_punctuation, trie_nodes)
-    return trie_nodes
+            populate_trie_nodes(f, N, include_punctuation, trie_node)
+    return trie_node
 
 def compute_trie_nodes_for_file(fn, N, include_punctuation=False):
     with open(fn, 'r') as f:
-        return make_trie_nodes(f, N, include_punctuation)
+        return populate_trie_nodes(f, N, include_punctuation)
 
-def compute_model(trie_nodes, N):
-    _add_discounts(trie_nodes, N)
-    _add_alphas(trie_nodes, N)
-    return trie_nodes
+def compute_model(trie_node, N):
+    _add_discounts(trie_node, N)
+    _add_alphas(trie_node, N)
+    return trie_node
--- katzbackoff_test.py
+++ katzbackoff_test.py
@@ -1,6 +1,6 @@
-import katzbackoff
+import ch4, katzbackoff
 import unittest
-import io
+import io, math
 
 class LanguageModelTests(unittest.TestCase):
     def test_trie_nodes(self):
@@ -9,5 +9,101 @@ class LanguageModelTests(unittest.TestCase):
             "Humpty Dumpty had a great fall; "
             "All the king's horses and all the king's men "
             "Couldn't put Humpty together again.")
-        trie_nodes = katzbackoff.make_trie_nodes(f, 3, False)
-        print(trie_nodes)
+        trie_node = katzbackoff.populate_trie_nodes(f, 3, False)
+        self.assertEqual(30, trie_node.count)
+        humpty_node = trie_node.descendants['humpty']
+        self.assertEqual(3, humpty_node.count)
+        self.assertEqual(('humpty',), humpty_node.n_gram)
+        self.assertEqual(2, len(humpty_node.descendants))
+        dumpty_node = humpty_node.descendants['dumpty']
+        together_node = humpty_node.descendants['together']
+        self.assertEqual(2, dumpty_node.count)
+        self.assertEqual(('humpty', 'dumpty',), dumpty_node.n_gram)
+        self.assertEqual(1, together_node.count)
+        self.assertEqual(2, len(dumpty_node.descendants))
+        sat_node = dumpty_node.descendants['sat']
+        self.assertEqual(1, sat_node.count)
+        self.assertEqual(('humpty', 'dumpty', 'sat',), sat_node.n_gram)
+        self.assertEqual(0, len(sat_node.descendants))
+        self.assertEqual(1, len(together_node.descendants))
+        self.assertEqual(0, len(together_node.descendants['again'].descendants))
+
+    def test_trie_nodes_terminal(self):
+        f = io.StringIO(
+            "Humpty Dumpty sat on a wall, "
+            "Humpty Dumpty had a great fall; "
+            "All the king's horses and all the king's men "
+            "Couldn't put Humpty together again.")
+        trie_node = katzbackoff.populate_trie_nodes(f, 3, False)
+        self.assertTrue('together' in trie_node.descendants)
+
+    def _n_gram_nodes(self, trie_node, n):
+        nodes = {}
+        stack = [trie_node]
+        while len(stack) > 0:
+            node = stack.pop()
+            if len(node.n_gram) == n:
+                nodes[node.n_gram] = node
+            elif len(node.n_gram) < n:
+                for d in node.descendants.values():
+                    stack.append(d)
+        return nodes
+
+    def test_c_star_bigrams(self):
+        f = io.StringIO(
+            "Humpty Dumpty sat on a wall, "
+            "Humpty Dumpty had a great fall; "
+            "All the king's horses and all the king's men "
+            "Couldn't put Humpty together again.")
+        trie_node = katzbackoff.compute_model(katzbackoff.populate_trie_nodes(f, 3), 3)
+        bigram_nodes = self._n_gram_nodes(trie_node, 2)
+        # idea here is to compare discounted counts to those obtained
+        # through other method
+        f.seek(0)
+        cfs = ch4.compute_n_gram_model(f, 2).n_gram_count_frequencies()
+        a, b = ch4.simple_linear_regression(cfs)
+        discount = ch4.SimpleGoodTuringCountSmoother(b)
+        for node in bigram_nodes.values():
+            self.assertEqual(discount(node.count), node.c_star())
+
+    def test_c_star_trigrams(self):
+        f = io.StringIO(
+            "Humpty Dumpty sat on a wall, "
+            "Humpty Dumpty had a great fall; "
+            "All the king's horses and all the king's men "
+            "Couldn't put Humpty together again.")
+        trie_node = katzbackoff.compute_model(katzbackoff.populate_trie_nodes(f, 3), 3)
+        trigram_nodes = self._n_gram_nodes(trie_node, 3)
+        # idea here is to compare discounted counts to those obtained
+        # through other method
+        f.seek(0)
+        cfs = ch4.compute_n_gram_model(f, 3).n_gram_count_frequencies()
+        a, b = ch4.simple_linear_regression(cfs)
+        discount = ch4.SimpleGoodTuringCountSmoother(b)
+        for node in trigram_nodes.values():
+            self.assertEqual(discount(node.count), node.c_star())
+
+    def test_alpha(self):
+        f = io.StringIO(
+            "Humpty Dumpty sat on a wall, "
+            "Humpty Dumpty had a great fall; "
+            "All the king's horses and all the king's men "
+            "Couldn't put Dumpty together again.") # note we changed Humpty -> Dumpty
+        trie_node = katzbackoff.compute_model(katzbackoff.populate_trie_nodes(f, 3), 3)
+        humpty_node = trie_node.descendants['humpty']
+        humpty_dumpty_node = humpty_node.descendants['dumpty']
+        dumpty_node = trie_node.descendants['dumpty']
+        # alpha(humpty dumpty)
+        beta_n_1 = 1.0 - sum(d.p_star() for d in humpty_dumpty_node.descendants.values())
+        denominator = 1.0 - sum(d.p_star() for d
+                                in dumpty_node.descendants.values()
+                                if d.n_gram[-1] in ['sat', 'had'])
+        self.assertAlmostEqual(
+            beta_n_1 / denominator, math.exp(humpty_dumpty_node.log_alpha), 4)
+        # alpha(dumpty)
+        beta_n_1 = 1.0 - sum(d.p_star() for d in dumpty_node.descendants.values())
+        denominator = 1.0 - sum(d.p_star() for d
+                                in trie_node.descendants.values()
+                                if d.n_gram[0] in ['sat', 'had', 'together'])
+        self.assertAlmostEqual(
+            beta_n_1 / denominator, math.exp(dumpty_node.log_alpha), 4)
