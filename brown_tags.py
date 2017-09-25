9d528927c110b7a8a9632ef173442666c54d4b4d
Adam Duston <aduston@gmail.com>
2017-09-25 09:27:55 -0400

    More work on transformation-based learning

--- brown_tags.py
+++ brown_tags.py
@@ -1,346 +1,87 @@
-table = """
-<table class="wikitable">
-<tbody>
-<tr>
-<td>.</td>
-<td>sentence (.&nbsp;;&nbsp;? *)</td>
-</tr>
-<tr>
-<td>(</td>
-<td>left paren</td>
-</tr>
-<tr>
-<td>)</td>
-<td>right paren</td>
-</tr>
-<tr>
-<td>*</td>
-<td>not, n't</td>
-</tr>
-<tr>
-<td>--</td>
-<td>dash</td>
-</tr>
-<tr>
-<td>,</td>
-<td>comma</td>
-</tr>
-<tr>
-<td>:</td>
-<td>colon</td>
-</tr>
-<tr>
-<td>ABL</td>
-<td>pre-qualifier (quite, rather)</td>
-</tr>
-<tr>
-<td>ABN</td>
-<td>pre-quantifier (half, all)</td>
-</tr>
-<tr>
-<td>ABX</td>
-<td>pre-quantifier (both)</td>
-</tr>
-<tr>
-<td>AP</td>
-<td>post-determiner (many, several, next)</td>
-</tr>
-<tr>
-<td>AT</td>
-<td>article (a, the, no)</td>
-</tr>
-<tr>
-<td>BE</td>
-<td>be</td>
-</tr>
-<tr>
-<td>BED</td>
-<td>were</td>
-</tr>
-<tr>
-<td>BEDZ</td>
-<td>was</td>
-</tr>
-<tr>
-<td>BEG</td>
-<td>being</td>
-</tr>
-<tr>
-<td>BEM</td>
-<td>am</td>
-</tr>
-<tr>
-<td>BEN</td>
-<td>been</td>
-</tr>
-<tr>
-<td>BER</td>
-<td>are, art</td>
-</tr>
-<tr>
-<td>BEZ</td>
-<td>is</td>
-</tr>
-<tr>
-<td>CC</td>
-<td>coordinating conjunction (and, or)</td>
-</tr>
-<tr>
-<td>CD</td>
-<td>cardinal numeral (one, two, 2, etc.)</td>
-</tr>
-<tr>
-<td>CS</td>
-<td>subordinating conjunction (if, although)</td>
-</tr>
-<tr>
-<td>DO</td>
-<td>do</td>
-</tr>
-<tr>
-<td>DOD</td>
-<td>did</td>
-</tr>
-<tr>
-<td>DOZ</td>
-<td>does</td>
-</tr>
-<tr>
-<td>DT</td>
-<td>singular determiner/quantifier (this, that)</td>
-</tr>
-<tr>
-<td>DTI</td>
-<td>singular or plural determiner/quantifier (some, any)</td>
-</tr>
-<tr>
-<td>DTS</td>
-<td>plural determiner (these, those)</td>
-</tr>
-<tr>
-<td>DTX</td>
-<td>determiner/double conjunction (either)</td>
-</tr>
-<tr>
-<td>EX</td>
-<td>existential there</td>
-</tr>
-<tr>
-<td>FW</td>
-<td>foreign word (hyphenated before regular tag)</td>
-</tr>
-<tr>
-<td>HV</td>
-<td>have</td>
-</tr>
-<tr>
-<td>HVD</td>
-<td>had (past tense)</td>
-</tr>
-<tr>
-<td>HVG</td>
-<td>having</td>
-</tr>
-<tr>
-<td>HVN</td>
-<td>had (past participle)</td>
-</tr>
-<tr>
-<td>IN</td>
-<td>preposition</td>
-</tr>
-<tr>
-<td>JJ</td>
-<td>adjective</td>
-</tr>
-<tr>
-<td>JJR</td>
-<td>comparative adjective</td>
-</tr>
-<tr>
-<td>JJS</td>
-<td>semantically superlative adjective (chief, top)</td>
-</tr>
-<tr>
-<td>JJT</td>
-<td>morphologically superlative adjective (biggest)</td>
-</tr>
-<tr>
-<td>MD</td>
-<td>modal auxiliary (can, should, will)</td>
-</tr>
-<tr>
-<td>NC</td>
-<td>cited word (hyphenated after regular tag)</td>
-</tr>
-<tr>
-<td>NN</td>
-<td>singular or mass noun</td>
-</tr>
-<tr>
-<td>NN$</td>
-<td>possessive singular noun</td>
-</tr>
-<tr>
-<td>NNS</td>
-<td>plural noun</td>
-</tr>
-<tr>
-<td>NNS$</td>
-<td>possessive plural noun</td>
-</tr>
-<tr>
-<td>NP</td>
-<td>proper noun or part of name phrase</td>
-</tr>
-<tr>
-<td>NP$</td>
-<td>possessive proper noun</td>
-</tr>
-<tr>
-<td>NPS</td>
-<td>plural proper noun</td>
-</tr>
-<tr>
-<td>NPS$</td>
-<td>possessive plural proper noun</td>
-</tr>
-<tr>
-<td>NR</td>
-<td>adverbial noun (home, today, west)</td>
-</tr>
-<tr>
-<td>OD</td>
-<td>ordinal numeral (first, 2nd)</td>
-</tr>
-<tr>
-<td>PN</td>
-<td>nominal pronoun (everybody, nothing)</td>
-</tr>
-<tr>
-<td>PN$</td>
-<td>possessive nominal pronoun</td>
-</tr>
-<tr>
-<td>PP$</td>
-<td>possessive personal pronoun (my, our)</td>
-</tr>
-<tr>
-<td>PP$$</td>
-<td>second (nominal) possessive pronoun (mine, ours)</td>
-</tr>
-<tr>
-<td>PPL</td>
-<td>singular reflexive/intensive personal pronoun (myself)</td>
-</tr>
-<tr>
-<td>PPLS</td>
-<td>plural reflexive/intensive personal pronoun (ourselves)</td>
-</tr>
-<tr>
-<td>PPO</td>
-<td>objective personal pronoun (me, him, it, them)</td>
-</tr>
-<tr>
-<td>PPS</td>
-<td>3rd. singular nominative pronoun (he, she, it, one)</td>
-</tr>
-<tr>
-<td>PPSS</td>
-<td>other nominative personal pronoun (I, we, they, you)</td>
-</tr>
-<tr>
-<td>PRP</td>
-<td>Personal pronoun</td>
-</tr>
-<tr>
-<td>PRP$</td>
-<td>Possessive pronoun</td>
-</tr>
-<tr>
-<td>QL</td>
-<td>qualifier (very, fairly)</td>
-</tr>
-<tr>
-<td>QLP</td>
-<td>post-qualifier (enough, indeed)</td>
-</tr>
-<tr>
-<td>RB</td>
-<td>adverb</td>
-</tr>
-<tr>
-<td>RBR</td>
-<td>comparative adverb</td>
-</tr>
-<tr>
-<td>RBT</td>
-<td>superlative adverb</td>
-</tr>
-<tr>
-<td>RN</td>
-<td>nominal adverb (here, then, indoors)</td>
-</tr>
-<tr>
-<td>RP</td>
-<td>adverb/particle (about, off, up)</td>
-</tr>
-<tr>
-<td>TO</td>
-<td>infinitive marker to</td>
-</tr>
-<tr>
-<td>UH</td>
-<td>interjection, exclamation</td>
-</tr>
-<tr>
-<td>VB</td>
-<td>verb, base form</td>
-</tr>
-<tr>
-<td>VBD</td>
-<td>verb, past tense</td>
-</tr>
-<tr>
-<td>VBG</td>
-<td>verb, present participle/gerund</td>
-</tr>
-<tr>
-<td>VBN</td>
-<td>verb, past participle</td>
-</tr>
-<tr>
-<td>VBP</td>
-<td>verb, non 3rd person, singular, present</td>
-</tr>
-<tr>
-<td>VBZ</td>
-<td>verb, 3rd. singular present</td>
-</tr>
-<tr>
-<td>WDT</td>
-<td>wh- determiner (what, which)</td>
-</tr>
-<tr>
-<td>WP$</td>
-<td>possessive wh- pronoun (whose)</td>
-</tr>
-<tr>
-<td>WPO</td>
-<td>objective wh- pronoun (whom, which, that)</td>
-</tr>
-<tr>
-<td>WPS</td>
-<td>nominative wh- pronoun (who, which, that)</td>
-</tr>
-<tr>
-<td>WQL</td>
-<td>wh- qualifier (how)</td>
-</tr>
-<tr>
-<td>WRB</td>
-<td>wh- adverb (how, where, when)</td>
-</tr>
-</tbody></table>
-"""
-
+TAGS = {
+    '(': 'left paren',
+    ')': 'right paren',
+    '*': "not, n't",
+    ',': 'comma',
+    '--': 'dash',
+    '.': 'sentence (.\xa0;\xa0? *)',
+    ':': 'colon',
+    'ABL': 'pre-qualifier (quite, rather)',
+    'ABN': 'pre-quantifier (half, all)',
+    'ABX': 'pre-quantifier (both)',
+    'AP': 'post-determiner (many, several, next)',
+    'AT': 'article (a, the, no)',
+    'BE': 'be',
+    'BED': 'were',
+    'BEDZ': 'was',
+    'BEG': 'being',
+    'BEM': 'am',
+    'BEN': 'been',
+    'BER': 'are, art',
+    'BEZ': 'is',
+    'CC': 'coordinating conjunction (and, or)',
+    'CD': 'cardinal numeral (one, two, 2, etc.)',
+    'CS': 'subordinating conjunction (if, although)',
+    'DO': 'do',
+    'DOD': 'did',
+    'DOZ': 'does',
+    'DT': 'singular determiner/quantifier (this, that)',
+    'DTI': 'singular or plural determiner/quantifier (some, any)',
+    'DTS': 'plural determiner (these, those)',
+    'DTX': 'determiner/double conjunction (either)',
+    'EX': 'existential there',
+    'FW': 'foreign word (hyphenated before regular tag)',
+    'HV': 'have',
+    'HVD': 'had (past tense)',
+    'HVG': 'having',
+    'HVN': 'had (past participle)',
+    'IN': 'preposition',
+    'JJ': 'adjective',
+    'JJR': 'comparative adjective',
+    'JJS': 'semantically superlative adjective (chief, top)',
+    'JJT': 'morphologically superlative adjective (biggest)',
+    'MD': 'modal auxiliary (can, should, will)',
+    'NC': 'cited word (hyphenated after regular tag)',
+    'NN': 'singular or mass noun',
+    'NN$': 'possessive singular noun',
+    'NNS': 'plural noun',
+    'NNS$': 'possessive plural noun',
+    'NP': 'proper noun or part of name phrase',
+    'NP$': 'possessive proper noun',
+    'NPS': 'plural proper noun',
+    'NPS$': 'possessive plural proper noun',
+    'NR': 'adverbial noun (home, today, west)',
+    'OD': 'ordinal numeral (first, 2nd)',
+    'PN': 'nominal pronoun (everybody, nothing)',
+    'PN$': 'possessive nominal pronoun',
+    'PP$': 'possessive personal pronoun (my, our)',
+    'PP$$': 'second (nominal) possessive pronoun (mine, ours)',
+    'PPL': 'singular reflexive/intensive personal pronoun (myself)',
+    'PPLS': 'plural reflexive/intensive personal pronoun (ourselves)',
+    'PPO': 'objective personal pronoun (me, him, it, them)',
+    'PPS': '3rd. singular nominative pronoun (he, she, it, one)',
+    'PPSS': 'other nominative personal pronoun (I, we, they, you)',
+    'PRP': 'Personal pronoun',
+    'PRP$': 'Possessive pronoun',
+    'QL': 'qualifier (very, fairly)',
+    'QLP': 'post-qualifier (enough, indeed)',
+    'RB': 'adverb',
+    'RBR': 'comparative adverb',
+    'RBT': 'superlative adverb',
+    'RN': 'nominal adverb (here, then, indoors)',
+    'RP': 'adverb/particle (about, off, up)',
+    'TO': 'infinitive marker to',
+    'UH': 'interjection, exclamation',
+    'VB': 'verb, base form',
+    'VBD': 'verb, past tense',
+    'VBG': 'verb, present participle/gerund',
+    'VBN': 'verb, past participle',
+    'VBP': 'verb, non 3rd person, singular, present',
+    'VBZ': 'verb, 3rd. singular present',
+    'WDT': 'wh- determiner (what, which)',
+    'WP$': 'possessive wh- pronoun (whose)',
+    'WPO': 'objective wh- pronoun (whom, which, that)',
+    'WPS': 'nominative wh- pronoun (who, which, that)',
+    'WQL': 'wh- qualifier (how)',
+    'WRB': 'wh- adverb (how, where, when)'
+}
--- transformation_based_learning.py
+++ transformation_based_learning.py
@@ -1,12 +1,76 @@
+import itertools
+from brown_tags import TAGS
+
 class Template(object):
     def __init__(self, z_spec, w_spec=None):
         self.z_spec = z_spec
         self.w_spec = w_spec
 
+    def make_candidate(self, corpus, position):
+        pass # TODO: left off here
+
 class TemplateInstanceCandidate(object):
     def __init__(self, z_set, w_set=None):
         self.z_set = z_set
         self.w_set = w_set
+        self._key = "|".join(sorted(list(z_set)))
+        if w_set is not None:
+            self._key += "||"
+            self._key += "|".join(sorted(list(w_set)))
+
+    def __hash__(self):
+        return self._key
+
+    def __eq__(self, other):
+        return self._key == other._key
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def matches_args(self, z, w=None):
+        if w is None:
+            return z in self.z_set
+        else:
+            return (z in self.z_set) and (w in self.w_set)
+
+class TemplateInstanceCandidates(object):
+    def __init__(self, template):
+        self.template = template
+        # mappings of TemplateInstanceCandidate -> count
+        self._good_candidates = {}
+        self._bad_candidates = {}
+
+    def add_good_candidate(self, candidate):
+        if candidate is None:
+            return
+        self._good_candidates[candidate] = \
+            self._good_candidates.get(candidate, 0) + 1
+
+    def add_bad_candidate(self, candidate):
+        if candidate is None:
+            return
+        self._bad_candidates[candidate] = \
+            self._bad_candidates.get(candidate, 0) + 1
+
+    def get_best(self):
+        args_iter = None
+        if self.template.get_num_args() == 1:
+            args_iter = ((t, None) for t in TAGS.keys())
+        else:
+            args_iter = itertools.product(TAGS.keys(), TAGS.keys())
+        best_args, best_args_score = None, -sys.maxsize
+        for z, w in args_iter:
+            good_candidates_score = sum(
+                v for k, v in self._good_candidates.items()
+                if k.matches_args(z, w))
+            bad_candidates_score = sum(
+                v for k, v in self._bad_candidates.items()
+                if k.matches_args(z, w))
+            score = good_candidates_score - bad_candidates_score
+            if score > best_args_score:
+                best_args = (z, w)
+                best_args_score = best_args_score
+        return best_args[0], best_args[1], best_args_score
 
 class TemplateInstance(object):
     def __init__(self, a, b, z, w=None):
@@ -14,8 +78,6 @@ class TemplateInstance(object):
         self.b = b
         self.z = z
         self.w = w
-
-TAGS = 
 
 TEMPLATES = [ # same templates Brill used
     Template(-1),
@@ -35,4 +97,20 @@ def get_best_instance(corpus, template):
     """
     Returns a (template_instance, score) pair.
     """
-    
+    best_instance, best_instance_score = None, -sys.maxsize
+    for from_tag, to_tag in itertools.product(TAGS.keys(), TAGS.keys()):
+        candidates = TemplateInstanceCandidates(template)
+        for i in range(corpus.size()):
+            if corpus.current_tag(i) == from_tag:
+                if corpus.correct_tag(i) == to_tag:
+                    candidates.add_good_candidate(
+                        template.make_candidate(corpus, i))
+                elif corpus.correct_tag(i) == from_tag:
+                    candidates.add_bad_candidate(
+                        template.make_candidate(corpus, i))
+        z, w, score = candidates.get_best()
+        if score > best_instance_score:
+            best_instance_score = score
+            best_instance = TemplateInstance(
+                from_tag, to_tag, z, w)
+    return best_instance, best_instance_score
